2e1b91e9f6dacc457bbbb3eb711032bd87e4aef2
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-06-15 13:46:18 -0300

    initial items model and controller set up

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     Rails.application.credentials[Rails.env.to_sym][:aws_access_key_id],                        # required unless using use_iam_profile
+    aws_secret_access_key: Rails.application.credentials[Rails.env.to_sym][:aws_secret_access_key],                        # required unless using use_iam_profile
+  }
+  config.fog_directory  =  Rails.application.credentials[Rails.env.to_sym][:aws_bucket]                                      # required
+end
