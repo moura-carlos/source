8dd2c881b4d007a882ed26ae6d087d252a605b45
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-06-16 10:12:34 -0300

    force login. Define function to force the vistitor to login/sign up. If they are not logged in, they view limited parts of the website.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
+  # checking that the user is logged in with the method in the application controller
+  # unless the user is logged in, they cannot see the items.
+  # the reason why we have direct access to #force_login is
+  # because ItemsController inherits from ApplicationController
+  # so it has access to its methods.
+  # if this was a view, to use that - #force_login - method,
+  # I would have to add it as a helper_method in the application
+  # controller so that I would have access to it in both controllers and views.
+  before_action :force_login
+
   def index
     @items = Item.all
   end
