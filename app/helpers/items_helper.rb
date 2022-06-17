65ef8d36cf731b884686831b091ec8ca21f0246e
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-06-17 14:53:54 -0300

    Add categories to the Source website

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,8 @@
 module ItemsHelper
+  def format_categories(categories)
+    links = categories.map do |category|
+      link_to category.title, category_path(category)
+    end
+    links.to_sentence.html_safe
+  end
 end
