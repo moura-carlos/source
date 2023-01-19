e3f113dfd04e497757202d6b3742e17a2fa1b20f
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-01-19 12:03:12 -0800

    carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',                        # required
     aws_access_key_id:     Rails.application.credentials[Rails.env.to_sym][:aws_access_key_id], # required unless using use_iam_profile
-    aws_secret_access_key: Rails.application.credentials[Rails.env.to_sym][:aws_secret_access_key] # required unless using use_iam_profile
+    aws_secret_access_key: Rails.application.credentials[Rails.env.to_sym][:aws_secret_access_key], # required unless using use_iam_profile
+    region: 'us-west-2'
   }
   config.fog_directory = Rails.application.credentials[Rails.env.to_sym][:aws_bucket] # required
 end
